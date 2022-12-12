cca277dc3d1b5b935471c723b409db104f5fd5f9
demutilator <>
2022-12-12 18:41:29 -0500

    Adds a Background, Sound, and Idle Animation

--- Elephant.java
+++ Elephant.java
@@ -8,7 +8,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+    
+    
+    String facing = "right";
+    public Elephant(){
+        for(int i = 0; i < idleRight.length;i++){
+            idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i  + ".png");
+            idleRight[i].scale(100,100);
+        }
+        setImage(idleRight[0]);
+    }
+    
+    int imageIndex = 0;
+    public void animateElephant(){
+        setImage(idleRight[imageIndex]);
+        imageIndex = (imageIndex + 1) % idleRight.length;
+    }
     
     public void act(){
         if(Greenfoot.isKeyDown("left")){
@@ -23,7 +41,7 @@ public class Elephant extends Actor
         int numberOfApples = 0;
         if(isTouching(Apple.class)){
             removeTouching(Apple.class);
-            
+            elephantSound.play();
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
             numberOfApples++;
