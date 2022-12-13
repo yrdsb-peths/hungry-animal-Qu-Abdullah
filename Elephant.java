02153cdf4a2ae57be2061dece2e9c36e108bc0d7
demutilator <>
2022-12-13 15:19:38 -0500

    Added Animations and a Score counter

--- Elephant.java
+++ Elephant.java
@@ -14,37 +14,64 @@ public class Elephant extends Actor
     
     
     String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
+    
     public Elephant(){
         for(int i = 0; i < idleRight.length;i++){
             idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i  + ".png");
             idleRight[i].scale(100,100);
         }
+        
+        for(int i = 0; i < idleLeft.length;i++){
+            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i  + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100,100);
+        }
+        
+        animationTimer.mark();
+        
         setImage(idleRight[0]);
     }
     
     int imageIndex = 0;
     public void animateElephant(){
-        setImage(idleRight[imageIndex]);
-        imageIndex = (imageIndex + 1) % idleRight.length;
+        if(animationTimer.millisElapsed() < 125){
+            return;
+        }
+        
+        animationTimer.mark();
+        
+        if(facing.equals("right")){
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        } 
+        else{
+           setImage(idleLeft[imageIndex]);
+           imageIndex = (imageIndex + 1) % idleLeft.length; 
+        }
     }
     
     public void act(){
         if(Greenfoot.isKeyDown("left")){
             move(-2);
+            facing = "left";
         } else if(Greenfoot.isKeyDown("right")){
             move(2);
+            facing = "right";
         }
         eat();
+        
+        animateElephant();
     }
     
     public void eat(){
-        int numberOfApples = 0;
+
         if(isTouching(Apple.class)){
             removeTouching(Apple.class);
             elephantSound.play();
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
-            numberOfApples++;
+            world.increaseScore();
     }
     }
     
